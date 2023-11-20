219042b38d20f74fdda2f8e56ea73ff21496c63c
莫良宝 <2205247884@qq.com>
2023-11-20 16:11:56 +0800

    init project

---
+++ chatgpt-openAi-interfaces/src/main/java/cn.mlb.ai.api/ApiAoolication.java
@@ -0,0 +1,15 @@
+package cn.mlb.ai.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动入口
+ */
+@SpringBootApplication
+
+public class ApiAoolication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiAoolication.class, args);
+    }
+}
---
+++ chatgpt-openAi-interfaces/src/test/java/cn/mlb/ai/api/test/ApiTest.java
@@ -0,0 +1,7 @@
+package cn.mlb.ai.api.test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApiTest {
+}
